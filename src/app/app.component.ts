d9655296f429551ae015c7967cce24da2e0366af
Dinesh Raja <dineshrajaa.93@gmail.com>
2019-07-17 23:24:59 +0530

    Logic for date range filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { extendMoment } from 'moment-range';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-recurrence';
+  frequencies = ['daily', 'weekly', 'weeklytwice', 'monthly', 'yearly'];
+  selectedFrequency = this.frequencies[0];
+  fromDate = '';
+  toDate = '';
+  generateDates() {
+    // to generate dates based on frequency
+    const startDate = moment(this.fromDate);
+    const toDate = moment(this.toDate);
+    const extendedMoment = extendMoment(moment);
+    if (this.selectedFrequency === 'daily') {
+      const dateRange = extendedMoment.range(startDate, toDate);
+      const arrayOfIntervalDates = Array.from(dateRange.by('day', { step: 1 }));
+      const weekdays = [0, 1, 2, 3];
+      const possibleDays = arrayOfIntervalDates.filter(date => weekdays.includes(date.day()));
+      possibleDays.forEach((possibleDay) => console.warn(possibleDay.format('DD-MMM-YYYY')));
+    }
+  }
+
+  assignDates(selectedDate, fieldName) {
+    if (fieldName === 'fromDate') {
+      this.fromDate = selectedDate.value;
+    } else {
+      this.toDate = selectedDate.value;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
